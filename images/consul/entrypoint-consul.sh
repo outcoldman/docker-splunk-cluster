3cd743430b3ba8721940083709b7cd094cd089b3
Denis Gladkikh <denis@gladkikh.email>
2016-07-29 09:30:48 -0700

    Working on Swarm example

--- images/consul/entrypoint-consul.sh
+++ images/consul/entrypoint-consul.sh
@@ -7,7 +7,6 @@
     -dc=${CONSUL_DC:-"dc"} \
     -domain=${CONSUL_DOMAIN:-"splunk"} \
     -server \
-    -ui \
     -client=${CONSUL_CLIENT:-"127.0.0.1"} \
     -advertise=$(ip addr list ${CONSUL_ADVERTISE_INTERFACE:-eth0} | grep "inet " | cut -d' ' -f6| cut -d/ -f1 | head -1) \
     -retry-join=${CONSUL_JOIN:-"consul"} \
--- images/lb/entrypoint-consul.sh
+++ images/lb/entrypoint-consul.sh
@@ -10,4 +10,6 @@ mkdir -p /var/consul/data
     -domain="${CONSUL_DOMAIN:-splunk}" \
     -advertise=$(ip addr list ${CONSUL_ADVERTISE_INTERFACE:-eth0} | grep "inet " | cut -d' ' -f6| cut -d/ -f1 | head -1) \
     -http-port=8499 \
+    -ui \
+    -client=127.0.0.1 \
     -config-file="/etc/consul/consul.json"
