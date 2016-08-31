3f096a8891afa3037fcbd162f1f3ddacda19f43e
Denis Gladkikh <denis@gladkikh.email>
2016-08-31 12:32:55 -0700

    Working on demo

--- images/splunk/deployment/init_shc_deployer.py
+++ images/splunk/deployment/init_shc_deployer.py
@@ -16,5 +16,6 @@ def configurations():
 
 def substitution():
     return {
-        "@SHCLUSTERING_PASS_4_SYMM_KEY@": os.environ.get("INIT_SHCLUSTERING_PASS_4_SYMM_KEY", "shclustering-changeme")
+        "@SHCLUSTERING_PASS_4_SYMM_KEY@": os.environ.get("INIT_SHCLUSTERING_PASS_4_SYMM_KEY", "shclustering-changeme"),
+        "@SHCLUSTERING_SHCLUSTER_LABEL@": os.environ.get("INIT_SHCLUSTERING_SHCLUSTER_LABEL", "shcluster")
     }
